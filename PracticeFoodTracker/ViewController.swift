b94baf3204ce11fd8da4dd947c39c4ae869f61fc
LezardWhiteGlint <30715385+LezardWhiteGlint@users.noreply.github.com>
2019-02-08 14:43:36 +0800

    Initial Commit

---
+++ PracticeFoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeFoodTracker
+//
+//  Created by Lezardvaleth on 2019/2/8.
+//  Copyright © 2019 Lezardvaleth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
